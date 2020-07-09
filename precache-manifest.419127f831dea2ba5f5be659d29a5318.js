79ae14b7e6ce2ef1095aec7370cc133238899b37
umairahmed88 <umairahmed_88@ymail.com>
2020-07-09 14:44:13 +0500

    Updates

---
+++ precache-manifest.419127f831dea2ba5f5be659d29a5318.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba8084b0d786b2663ef38a6875932bb2",
+    "url": "/prc6/index.html"
+  },
+  {
+    "revision": "359677dfee4e70ea2bd6",
+    "url": "/prc6/static/css/main.39c186e3.chunk.css"
+  },
+  {
+    "revision": "deb8b457f32a793f2cc2",
+    "url": "/prc6/static/js/2.b9b95913.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/prc6/static/js/2.b9b95913.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "359677dfee4e70ea2bd6",
+    "url": "/prc6/static/js/main.1555ab7d.chunk.js"
+  },
+  {
+    "revision": "75834f0229e46f41b470",
+    "url": "/prc6/static/js/runtime-main.27a72f0d.js"
+  }
+]);
